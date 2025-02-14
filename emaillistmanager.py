957146599e34ad27784ff0cb4c82e60262485f6c
Bernadette Modi <berniemain353@gmail.com>
2025-02-14 12:29:17 +0300

    Added a class that checks if the email is valid or not and adds it to the email list.

--- emaillistmanager.py
+++ emaillistmanager.py
@@ -1,10 +1,27 @@
 import re
 from datetime import datetime 
 
-email = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+Reg_email = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
 
 def log(func):
     def wrapper(*args, **kwargs):
         print(f"[LOG] {func.__name__} called at {datetime.now()}")
         returen func(*args, **kwargs)
     return wrapper
+
+class EmailList:
+    def __init__(self):
+        self.emails = []
+    
+    def __iter__(self):
+        return iter(self.emails)
+
+    @log
+    def add_email(self, *emails):
+        """Adds multiple emails to the list after validation"""
+        for email in emails:
+            if re.fullmatch(Reg_email, email):
+                self.emails.append(email)
+                print(f"✅ Added: {email}")
+            else:
+                print(f"❌ Invalid email: {email}")
