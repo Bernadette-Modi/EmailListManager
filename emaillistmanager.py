ac8d77b79ba78eb9ed1e8d961c38e1756927302a
Bernadette Modi <berniemain353@gmail.com>
2025-02-14 12:20:07 +0300

    Created a decorator that logs function calls with timestamps.

--- emaillistmanager.py
+++ emaillistmanager.py
@@ -3,3 +3,8 @@ from datetime import datetime
 
 email = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
 
+def log(func):
+    def wrapper(*args, **kwargs):
+        print(f"[LOG] {func.__name__} called at {datetime.now()}")
+        returen func(*args, **kwargs)
+    return wrapper
